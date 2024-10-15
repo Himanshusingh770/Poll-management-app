d32d49c8522df0a2dd0d4b3c44da25f2f5c413de
Himanshu <himanshu.singh@innotechteam.in>
2024-10-15 14:30:36 +0530

    remove backgroung color

--- src/modules/Forms/Components/LoginForm.jsx
+++ src/modules/Forms/Components/LoginForm.jsx
@@ -2,14 +2,17 @@ import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { login } from '../../../slices/authSlice';
 import { useNavigate } from 'react-router-dom';
-import { Form, Button, Container, Card, Spinner, } from 'react-bootstrap';
+import { Form, Button, Container, Card, Spinner } from 'react-bootstrap';
 import { EyeFill, EyeSlashFill } from 'react-bootstrap-icons';
 import { isValidEmail } from '../../../utils/validationUtils';
 import ToastMessage from '../Components/ToastMessage';
 
 const Login = () => {
   const [formData, setFormData] = useState({ email: '', password: '' });
-  const [formFieldTouched, setFormFieldTouched] = useState({ email: false, password: false });
+  const [formFieldTouched, setFormFieldTouched] = useState({
+    email: false,
+    password: false
+  });
   const [showPassword, setShowPassword] = useState(false);
   const [showErrorToast, setShowErrorToast] = useState(false);
 
@@ -21,7 +24,8 @@ const Login = () => {
   const validateEmail = () => {
     if (!formFieldTouched.email) return '';
     if (!formData.email) return 'Email is required';
-    if (!isValidEmail(formData.email)) return 'Please enter a valid email address';
+    if (!isValidEmail(formData.email))
+      return 'Please enter a valid email address';
     return '';
   };
 
@@ -35,14 +39,20 @@ const Login = () => {
   const emailError = validateEmail();
   const passwordError = validatePassword();
 
-  const isFormValid = !emailError && !passwordError && formFieldTouched.password;
+  const isFormValid =
+    !emailError && !passwordError && formFieldTouched.password;
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    setFormFieldTouched({ ...formFieldTouched, password: true, email:true });
+    setFormFieldTouched({ ...formFieldTouched, password: true, email: true });
 
     if (isFormValid) {
-      dispatch(login({ email: formData.email.trim(), password: formData.password.trim() }))
+      dispatch(
+        login({
+          email: formData.email.trim(),
+          password: formData.password.trim()
+        })
+      )
         .unwrap()
         .then(() => navigate('/polls'))
         .catch(() => setShowErrorToast(true));
@@ -59,8 +69,15 @@ const Login = () => {
   const togglePasswordVisibility = () => setShowPassword(!showPassword);
 
   return (
-    <Container className="d-flex justify-content-center align-items-center vh-100 bg-primary">
-      <Card style={{ width: '30rem', borderRadius: '10px', backgroundColor: '#f8f9fa' }} className="shadow-lg p-4">
+    <Container className="d-flex justify-content-center align-items-center vh-100">
+      <Card
+        style={{
+          width: '30rem',
+          borderRadius: '10px',
+          backgroundColor: '#f8f9fa'
+        }}
+        className="shadow-lg p-4"
+      >
         <h2 className="text-center mb-4">Login</h2>
 
         <Form noValidate onSubmit={handleSubmit}>
@@ -74,31 +91,43 @@ const Login = () => {
               onChange={handlePasswordEmailChange}
               isInvalid={!!emailError}
             />
-            <Form.Control.Feedback type="invalid">{emailError}</Form.Control.Feedback>
+            <Form.Control.Feedback type="invalid">
+              {emailError}
+            </Form.Control.Feedback>
           </Form.Group>
 
           <Form.Group controlId="formPassword" className="mb-3">
             <Form.Label>Password</Form.Label>
             <div className="position-relative">
-              <Form.Control className='pe-3'
+              <Form.Control
+                className="pe-3"
                 type={showPassword ? 'text' : 'password'}
                 placeholder="Password"
                 name="password"
                 value={formData.password}
                 onChange={handlePasswordEmailChange}
-                onBlur={() => setFormFieldTouched((prevTouched) => ({ ...prevTouched, password: true }))}
+                onBlur={() =>
+                  setFormFieldTouched((prevTouched) => ({
+                    ...prevTouched,
+                    password: true
+                  }))
+                }
                 isInvalid={!!passwordError}
                 // style={{ paddingRight: '2.5rem' }}
               />
               <span
-                className={`position-absolute end-0 top-50 translate-middle-y d-flex justify-content-center align-items-center ${passwordError ? 'mx-5' : 'mx-3'}`}
+                className={`position-absolute end-0 top-50 translate-middle-y d-flex justify-content-center align-items-center ${
+                  passwordError ? 'mx-5' : 'mx-3'
+                }`}
                 style={{ cursor: 'pointer' }}
                 onClick={togglePasswordVisibility}
               >
                 {showPassword ? <EyeFill /> : <EyeSlashFill />}
               </span>
             </div>
-            {passwordError && <div className="text-danger mt-1">{passwordError}</div>}
+            {passwordError && (
+              <div className="text-danger mt-1">{passwordError}</div>
+            )}
           </Form.Group>
 
           <Button
@@ -115,7 +144,7 @@ const Login = () => {
       <ToastMessage
         show={showErrorToast}
         onClose={() => setShowErrorToast(false)}
-        message={error || "An error occurred"}
+        message={error || 'An error occurred'}
         variant="danger"
       />
     </Container>
--- src/slices/authSlice.jsx
+++ src/slices/authSlice.jsx
@@ -1,46 +1,51 @@
-// src/slices/authSlice.js
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import apiClient from '../utils/apiClient';
 // Async thunk for login
 
-export const login = createAsyncThunk('auth/login', async (credentials, thunkAPI) => {
-  try {
-    // Make an API call to your login endpoint using the Axios instance
-    const response = await apiClient.post('/user/login', {
-      email: credentials.email,
-      password: credentials.password,
-    });
-    const userData = response.data;
+export const login = createAsyncThunk(
+  'auth/login',
+  async (credentials, thunkAPI) => {
+    try {
+      // Make an API call to your login endpoint using the Axios instance
+      const response = await apiClient.post('/user/login', {
+        email: credentials.email,
+        password: credentials.password
+      });
+      const userData = response.data;
 
-    // Return the user data as the fulfilled action payload
+      // Return the user data as the fulfilled action payload
 
-    return {
-      email: userData.email,
-      name: userData.name,
-    };
+      return {
+        email: userData.email,
+        name: userData.name
+      };
+    } catch (error) {
+      console.error('API call failed: ', error); // Log the full error
 
-  } catch (error) {
-    console.error("API call failed: ", error); // Log the full error
-
-    // Log detailed error response if available
-    if (error.response) {
-      console.log("Error response data: ", error.response.data);
-      console.log("Error status: ", error.response.status);
-      console.log("Error headers: ", error.response.headers);
-      return thunkAPI.rejectWithValue(error.response.data.message || 'Login failed');
-    } else {
-      return thunkAPI.rejectWithValue('Something went wrong. Please try again.');
+      // Log detailed error response if available
+      if (error.response) {
+        console.log('Error response data: ', error.response.data);
+        console.log('Error status: ', error.response.status);
+        console.log('Error headers: ', error.response.headers);
+        return thunkAPI.rejectWithValue(
+          error.response.data.message || 'Login failed'
+        );
+      } else {
+        return thunkAPI.rejectWithValue(
+          'Something went wrong. Please try again.'
+        );
+      }
     }
   }
-});
+);
 
 const authSlice = createSlice({
   name: 'auth',
   initialState: {
-    user: null,  // Initially, no user is stored
-    isAuthenticated: false,  // User is not authenticated by default
+    user: null, // Initially, no user is stored
+    isAuthenticated: false, // User is not authenticated by default
     isLoading: false,
-    error: null,
+    error: null
   },
   extraReducers: (builder) => {
     builder
@@ -58,6 +63,6 @@ const authSlice = createSlice({
         state.isAuthenticated = false;
         state.error = action.payload;
       });
-  },
+  }
 });
 export default authSlice.reducer;
