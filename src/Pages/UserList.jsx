8169958d31b3ae4babc6916997b9501ed4006985
Himanshu <himanshu.singh@innotechteam.in>
2024-12-07 15:15:14 +0530

    Add loaderin user list

--- src/Pages/UserList.jsx
+++ src/Pages/UserList.jsx
@@ -37,7 +37,10 @@ const UserList = () => {
   </div>
 
   {isLoading ? (
-    <div className="text-center">Loading...</div>
+    <div className="flex justify-center items-center">
+    <div className="border-4 border-t-4 border-gray-200 border-t-blue-500 rounded-full w-12 h-12 animate-spin"></div>
+  </div>
+  
   ) : error ? (
     <div className="text-center text-red-500">{error}</div>
   ) : (
